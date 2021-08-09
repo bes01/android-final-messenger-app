745a9ab79009066fe1f2135474025927e3b7095e
tato <tkats18@freeuni.edu.ge>
2021-08-09 22:56:21 +0400

    optimize import

--- app/src/main/java/ge/bkapa/tkats/messengerapp/adapter/MessagePageFragmentAdapter.kt
+++ app/src/main/java/ge/bkapa/tkats/messengerapp/adapter/MessagePageFragmentAdapter.kt
@@ -3,8 +3,8 @@ package ge.bkapa.tkats.messengerapp.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import ge.bkapa.tkats.messengerapp.view.MessageListActivity
-import ge.bkapa.tkats.messengerapp.view.fragment.ProfileFragment
 import ge.bkapa.tkats.messengerapp.view.fragment.MessageListFragment
+import ge.bkapa.tkats.messengerapp.view.fragment.ProfileFragment
 
 class MessagePageFragmentAdapter(activity: MessageListActivity, userId: String) : FragmentStateAdapter(activity) {
 
